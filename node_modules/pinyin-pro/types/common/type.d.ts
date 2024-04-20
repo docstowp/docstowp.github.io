a567d34bcde27fc13466e8a2d65a08ae1df81e6b
vikramaruchamy <aruchamyvikram@gmail.com>
2024-04-20 18:47:29 +0530

    new version

---
+++ node_modules/pinyin-pro/types/common/type.d.ts
@@ -0,0 +1,9 @@
+export interface SingleWordResult {
+    origin: string;
+    originPinyin: string;
+    result: string;
+    isZh: boolean;
+    delete?: boolean;
+}
+export type ToneType = 'symbol' | 'num' | 'none';
+export type PinyinMode = 'normal' | 'surname';
